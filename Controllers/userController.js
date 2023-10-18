17c7215936f90579d9754d984d4f022ccc44c0d3
Meshak Otieno <meshakotieno343@gmail.com>
2023-10-18 17:17:29 +0300

    second commit

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,5 +1,5 @@
 // controllers/authController.js
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const User = require('../Models/User');
 
 const jwt = require('jsonwebtoken');
